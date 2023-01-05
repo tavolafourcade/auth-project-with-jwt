d60468f0f333a9e6bdee59b765075160f0ddb60c
tavolafourcade <tavolafourcade@gmail.com>
2023-01-05 10:02:22 -0500

    [ ADD ] Implementing a basic route for showing data from the package.json

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
 import express from 'express'
+import morgan from 'morgan'
+import fs from 'fs'
+
+const pkg = JSON.parse(fs.readFileSync('/Users/octaviolafourcade/Documents/PROYECTOS_PERSONAL/auth-project-with-jwt/package.json', 'utf8'))
 
 const app = express()
+
+app.set('pkg', pkg) // Sirve para colocarle un nombre a una variable y un valor
+
+app.use(morgan('dev'))
+
+// Routes
+app.get('/', (req,res) => {
+  res.json({
+    name: pkg.name,
+    author: app.get('pkg').author,
+    description: app.get('pkg').description,
+    version: app.get('pkg').version
+  })
+})
 
 export default app
