06dcce8b1d408314d4cf7edab1bd9e8637eff41b
tavolafourcade <tavolafourcade@gmail.com>
2023-01-06 09:44:28 -0500

    [ ADD ] Additional implementation for MongoDb connection

--- src/database.js
+++ src/database.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
 import * as dotenv from 'dotenv'
 
+dotenv.config()
+
 mongoose.connect(process.env.MONGO_URL)
+  .then(db => console.log('Db connected'))
+  .catch(error => console.log(error))
