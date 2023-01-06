cdfd6918b2234dab1e5410ac78e6faa509194b2d
tavolafourcade <tavolafourcade@gmail.com>
2023-01-06 09:46:17 -0500

    [ ADD ] Additional properties passed to the connection to avoid warnings

--- src/database.js
+++ src/database.js
@@ -3,6 +3,10 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(db => console.log('Db connected'))
   .catch(error => console.log(error))
