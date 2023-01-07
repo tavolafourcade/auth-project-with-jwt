fa475c6668a8bdd0ac2b7e245cc1984220288ea2
tavolafourcade <tavolafourcade@gmail.com>
2023-01-07 09:57:09 -0500

    [ ADD ] Implementation of the CRUD for Products

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -8,18 +8,24 @@ export const createProduct = async (req, res) => {
   res.status(201).json(productSaved)
 }
 
-export const getProducts = (req, res) => {
-  res.json('get products')
+export const getProducts = async (req, res) => {
+  const products = await Product.find()
+  res.json(products)
 }
 
-export const getProductById = (req, res) => {
-  res.json('get product by id')
+export const getProductById = async (req, res) => {
+  const product = await Product.findById(req.params.id)
+  res.status(200).json(product)
 }
 
-export const updateProductById = (req, res) => {
-  res.json('update product by id')
+export const updateProductById = async (req, res) => {
+  const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true
+  }) // Por defecto se devuelve el dato viejo, agregar new:true para que retorne el dato nuevo
+  res.status(200).json(updatedProduct)
 }
 
-export const deleteProductById = (req, res) => {
-  res.json('delete product by id')
+export const deleteProductById = async (req, res) => {
+  const deletedProduct = await Product.findByIdAndDelete(req.params.id)
+  res.status(204).json() // Con el 204 no devuelve nada pero ya nos confirma que es satisfactorio
 }
