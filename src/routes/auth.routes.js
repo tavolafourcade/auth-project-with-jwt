531207c1384733e1bbe4059e329950ebc2d5560b
tavolafourcade <tavolafourcade@gmail.com>
2023-01-08 10:14:32 -0500

    [ ADD ] Auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,4 +3,7 @@ import { Router } from "express"
 
 const router = Router()
 
+router.post('/signup') // Ruta para registrarse
+router.post('/signin') // Ruta para ingresar a la aplicación
+
 export default router
