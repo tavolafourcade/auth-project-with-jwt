c95882b5af60607841fdc777689b5ce1b0e5514d
tavolafourcade <tavolafourcade@gmail.com>
2023-01-08 11:17:43 -0500

    [ UPDATE ] User Model schema structure [ ADD ] methods to encrypt password and compare it

--- src/models/User.js
+++ src/models/User.js
@@ -1,26 +1,39 @@
 import { Schema, model, trusted } from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = new Schema({
   username: {
     type: String,
-    unique: trusted,
-    email: {
-      type: String,
-      unique: true
+    unique: true,
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  roles: [
+    {
+    ref: 'Role',
+    type: Schema.Types.ObjectId
     },
-    password: {
-      type: String,
-      required: true
-    },
-    roles: [
-      {
-      ref: 'Role',
-      type: Schema.Types.ObjectId
-      },
-    ],
-    timestamps: true,
-    versionKey: false
-  }
+  ]
+},
+{
+  timestamps: true,
+  versionKey: false
 })
 
-export default userSchema;
+// Generando un texto cifrado a partir de la contraseña
+userSchema.statics.encryptPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10)
+  return await bcrypt.hash(password, salt)
+}
+
+// Comparando la contraseña cifrada con la contraseña que se recibe
+userSchema.statics.comparePassword = async (password, receivedPassword) => {
+  await bcrypt.compare(password, receivedPassword) // Si las contraseñas coinciden retorna un true
+}
+export default model('User', userSchema)
