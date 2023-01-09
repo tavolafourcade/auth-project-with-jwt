fe3b514ae631d0ae44b046d3eb4f74160235d2d1
tavolafourcade <tavolafourcade@gmail.com>
2023-01-08 20:51:35 -0500

    [ ADD ] Implementation of validation for duplicated username or email and if the role exists

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 // Endpoints de Login y Register
 import { Router } from "express"
 import * as authCtrl from '../controllers/auth.controller.js'
+import { checkDuplicateUserNameOrEmail, checkRolesExisted } from '../middlewares/verifySignup.js'
 const router = Router()
 
-router.post('/signup', authCtrl.signUp) // Ruta para registrarse
+router.post('/signup', [checkDuplicateUserNameOrEmail, checkRolesExisted], authCtrl.signUp) // Ruta para registrarse
 router.post('/signin', authCtrl.signIn) // Ruta para ingresar a la aplicación
 
 export default router
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express"
-
+import * as userCtrl from '../controllers/user.controller.js'
+import { verifyToken, isModerator, isAdmin } from '../middlewares/auth.jwt.js'
+import { checkRolesExisted } from '../middlewares/verifySignup.js'
 const router = Router()
 
+router.post('/', [ verifyToken, isAdmin, checkRolesExisted ],userCtrl.createUser) // Solo el Admin podría crear nuevos usuarios
 export default router
